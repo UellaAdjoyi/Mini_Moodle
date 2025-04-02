afaee232bf5b030b7240d74159eaa5e749f2ea13
louise <adikpakpabiapiguedinessolouise@gmail.com>
2025-04-03 00:45:45 +0200

    mise à je la page de contenu d'une ue

--- view/student/page_ue.php
+++ view/student/page_ue.php
@@ -4,11 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page UE</title>
-    <link rel="stylesheet" href="style.css">
-    <script src="script.js" defer></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../styles/page_ue.css">
+    <link rel="stylesheet" href="../../bootstrap-5.0.2-dist/css/bootstrap.min.css">    
+    <script src="../../ajax/ue.js" defer></script>
 </head>
 <body>
+    <nav>
+        <?php
+        include('../../pageParts/navbarEtProf.php');
+        ?>
+    </nav>
     <header class="container my-3">
         <h1>UE : Nom de l'UE</h1>
         <button class="btn btn-primary float-end" id="createPostBtn" style="display: none;">Créer un nouveau post</button>
