96b71ba3ddb19c7f3b0c9b984fcf08e5edc6c0b6
UellaAdjoyi <uellaadjoyi07@gmail.com>
2025-04-04 11:36:12 +0200

    changement favicon

--- pageParts/link.php
+++ pageParts/link.php
@@ -1,4 +1,4 @@
 <link rel="stylesheet" href="/Projet_WE4A/styles/navbarAdmin.css">
 <link rel="stylesheet" href="/Projet_WE4A/styles/footer.css">
 <link rel="stylesheet" href="/Projet_WE4A/bootstrap-5.0.2-dist/css/bootstrap.min.css">
-<link rel="shortcut icon" href="/Projet_WE4A/assets/images/utbm.png" type="image/x-icon">
+<link rel="shortcut icon" href="/Projet_WE4A/assets/images/siteIcon.png" type="image/x-icon">
---
+++ view/profAdmin/home_page.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prof admin page</title>
+    <?php
+    include('../../pageParts/link.php')
+    ?>
+</head>
+
+<body>
+    <?php
+    include('../../pageParts/navbarProfAdmin.php')
+    ?>
+</body>
+
+</html>
